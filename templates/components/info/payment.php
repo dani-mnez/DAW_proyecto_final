a007b7a75f4a7bef9832b570fdddb08e01ba0e3b
AryanteChan <catalina.pc.chan@hotmail.com>
2023-03-04 20:58:52 +0100

    Página INFO pagos completada

--- templates/components/info/payment.php
+++ templates/components/info/payment.php
@@ -1,10 +1,28 @@
 <?php include_once(__DIR__ . '/../header.php'); ?>
     <div id="content">
-        <h1>Política de Pagos</h1>
-        <p>Cut the cheese cut the cheese airedale. Camembert de normandie croque monsieur brie danish fontina queso cottage cheese stilton mascarpone. Mascarpone gouda feta cheesecake stilton cheese on toast fondue squirty cheese. Brie bavarian bergkase mascarpone melted cheese cheese strings port-salut macaroni cheese red leicester. Cheddar macaroni cheese.</p>
-        <p>Port-salut roquefort say cheese. Port-salut cheeseburger smelly cheese cheese on toast cheesecake parmesan feta paneer. Mozzarella camembert de normandie cauliflower cheese monterey jack bavarian bergkase jarlsberg edam chalk and cheese. Jarlsberg dolcelatte ricotta taleggio.</p>
-        <p>Danish fontina gouda airedale. Cut the cheese parmesan fondue lancashire cheese and wine fondue red leicester gouda. Cheese and biscuits melted cheese camembert de normandie cow boursin melted cheese blue castello cheeseburger. Melted cheese everyone loves.</p>
-        <p>Lancashire danish fontina everyone loves. Pecorino say cheese cheese slices jarlsberg fromage dolcelatte cheese strings croque monsieur. Parmesan manchego caerphilly cheese and wine cheese strings pepper jack cheese strings cheese triangles. Stilton fromage frais smelly cheese cheese on toast lancashire.</p>
-        <p>Emmental fromage paneer. Cheese and wine paneer stilton cheesy feet danish fontina mozzarella cheese triangles camembert de normandie. Red leicester cream cheese fromage frais macaroni cheese st. agur blue cheese the big cheese cow hard cheese. Rubber cheese rubber cheese cheesy feet lancashire blue castello parmesan who moved my cheese blue castello. Cheese and wine cheese and wine everyone loves cheesecake.</p>
+        <h1>Formas de pago</h1>
+        <p>Elige la forma de pago que m&aacute;s te convenga.<br>El pago es 100% seguro. En Sol de Marzo no tenemos acceso a los datos bancarios de los clientes, trabajamos con entornos totalmente seguros. Adem&aacute;s, tienes varias formas de pago como:<br><br>Disponemos de las siguientes formas de pago:</p>
+        <ul>
+            <li>Pago con Tarjeta</li>
+            <li>Pago con Paypal</li>
+            <li>Pago con Bizum</li>
+            <li>Pago contra-reembolso</li>
+        </ul>
+        <h2><a name="tarjetas">1) Pago con Tarjeta</a></h2>
+        <p><img src="/DAW_PROYECTO_FINAL/assets/img/tarjetas.png" alt="Pagos con tarjeta"></p>
+        <p>Podr&aacute;s pagar tus compras con tarjeta VISA, Mastercard o American Express, a trav&eacute;s de nuestra pasarela de pago de Stripe.</p>
+        <p>El cargo se realiza online, es decir, en tiempo real, a trav&eacute;s de la pasarela de pago de la entidad financiera correspondiente y una vez se haya comprobado que los datos comunicados son correctos.</p>
+        <p>Con el objetivo de dar la m&aacute;xima seguridad al sistema de pago, utilizamos sistemas de pago seguro de entidades financieras de primera l&iacute;nea en comercio electr&oacute;nico. En este sentido, los datos confidenciales son transmitidos directamente y de forma encriptada (SSL) a la entidad financiera correspondiente. El sistema de encriptaci&oacute;n SSL que utilizamos confiere total seguridad a la transmisi&oacute;n de datos a trav&eacute;s de la red. <strong>Sus datos gozan de total confidencialidad y protecci&oacute;n.</strong></p>
+        <p><strong>En ning&uacute;n caso Sol de Marzo almacena los datos de su tarjeta.</strong> Son &uacute;nicamente utilizados en el TPV (Terminal Punto de Venta) virtual de nuestra entidad financiera, a trav&eacute;s de su Pasarela de Pago Seguro (redsys). Las tarjetas de cr&eacute;dito estar&aacute;n sujetas a comprobaciones y autorizaciones por parte de la entidad emisora de las mismas, pero si dicha entidad no autorizase el pago, no podremos hacernos responsables por ning&uacute;n retraso o falta de entrega.</p>
+        <h2><a name="paypal">2) Pago con Paypal</a></h2>
+        <p><img src="/DAW_PROYECTO_FINAL/assets/img/paypal.png" alt="Pagos con Paypal"></p>
+        <p>Si tienes una cuenta Paypal, tambi&eacute;n dispones de la opci&oacute;n de realizar el pago con esta modalidad, simplemente tienes que seleccionarla en el momento de completar el pedido y te redirigiremos a la plataforma de Paypal para completar el pago.</p>
+        <h2><a name="bizum">3) Pago con</a> Bizum</h2>
+        <p><img src="/DAW_PROYECTO_FINAL/assets/img/bizum.png" alt="Pagos con Bizum"></p>
+        <p>Si lo prefieres, tambi&eacute;n puedes realizar el pago a trav&eacute;s de Bizum.&nbsp;</p>
+        <p><strong>IMPORTANTE:&nbsp;</strong>Con esta modalidad de pago, tu pedido queda pendiente de confirmaci&oacute;n y te mandaremos un email con los datos para completar el pago. Una vez confirmado el pago, el pedido ya se completa &iexcl;Y nos ponemos manos en la masa para entreg&aacute;rtelo!</p>
+        <h2><a name="contrareembolso">3) Pago contra-reembolso</a></h2>
+        <p><img src="/DAW_PROYECTO_FINAL/assets/img/contrareembolso.png" alt="Pagos contra-reembolso"></p>
+        <p>Esta modalidad de pago solo se dar&aacute; en algunos de nuestros productores que realizan su propio env&iacute;o y recibir&aacute;n el pago en el momento de la entrega.</p>
     </div>
 <?php include_once(__DIR__ . '/../footer.php'); ?>
