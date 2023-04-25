d42dc6fea0ee56ce6cf7ed1ac9bb6e55c6745f22
Dani Martínez <reaprzzz@gmail.com>
2023-04-25 10:58:30 +0200

    Estilos, limpieza de código y ajustes de php en cart

--- templates/checkout.php
+++ templates/checkout.php
@@ -1,14 +1,10 @@
 <?php include_once(__DIR__ . '/components/header.php'); ?>
 <div id="content">
-    <div id="checkout_header">
-        <h1>Tramitar Pedido</h1>
-    </div>
+    <h1 id="page_title">Tramitar pedido</h1>
     <div id="checkout_content">
         <div id="col_left">
             <div id="addresses">
-                <div>
-                    <h2>Direcciones de Envío</h2>
-                </div>
+                <h2>Direcciones de envío</h2>
                 <div id="directions_wrapper">
                     <?php
                     foreach ($user_data->address as $address) {
@@ -28,149 +24,139 @@
                 </div>
             </div>
             <div id="pay_methods">
-                <div>
-                    <h2>Métodos de Pago</h2>
-                </div>
-                <div id="pay_methods_wrapper">
-                    <div id="pay_wrapper">
-                        <div class="payment_method">
-                            <img src="../assets/img/tarjetas2.svg" alt="tarjetas">
-                            <p><span>Pago con tarjeta bancaria</span></p>
-                        </div>
-                        <div class="payment_method">
-                            <img src="../assets/img/transferencia-bancaria2.svg" alt="transferencia">
-                            <p><span>Transferencia bancaria</span></p>
-                        </div>
-                        <div class="payment_method">
-                            <img id="img_bizum" src="../assets/img/bizum2.svg" alt="bizum">
-                            <p><span>Pago mediante Bizum</span></p>
-                        </div>
-                        <div class="payment_method">
-                            <img src="../assets/img/paypal2.svg" alt="paypal">
-                            <p><span>Pago mediante Paypal</span></p>
-                        </div>
+                <h2>Métodos de pago</h2>
+                <div id="pay_wrapper">
+                    <div class="payment_method">
+                        <img src="../assets/img/tarjetas.png" alt="tarjetas">
+                        <p>Tarjeta de crédito / débito</p>
+                    </div>
+                    <div class="payment_method">
+                        <img src="../assets/img/transferencia-bancaria.png" alt="transferencia">
+                        <p>Transferencia bancaria</p>
+                    </div>
+                    <div class="payment_method">
+                        <img src="../assets/img/bizum.png" alt="bizum">
+                    </div>
+                    <div class="payment_method">
+                        <img src="../assets/img/paypal.png" alt="paypal">
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <h2>Resumen del pedido y Opciones de envío</h2>
-                </div>
+            <div id="res_opts">
+                <h2>Resumen del pedido y opciones de envío</h2>
                 <div id="resume_wrapper">
                     <div id="resume_wrapper_items">
                         <?php
-                            $user = unserialize($_SESSION['user']);
-                            $carrito = $mongo_db->exec(
-                                'find_one',
-                                'users',
-                                ['_id' => new MongoDB\BSON\ObjectId(unserialize($_SESSION['user'])->id)]
-                            )->cart;
-                            foreach($carrito as $prod_carrito){
-                                if($prod_carrito->selected){
-                                    $prod_info = $mongo_db->exec(
-                                        'find_one',
-                                        'products',
-                                        ['_id' => $prod_carrito->product]
-                                    );
-                                    $producerName = $mongo_db->exec(
-                                        'find_one',
-                                        'producers',
-                                        ['_id' => $prod_info->producer]
-                                    )->company_name;
-                                    $price = $prod_info->stock[$prod_carrito->size]->price;
-                                    
-                                    require(__DIR__ . './components/checkout/prod_checkout.php');
-                                }
+                        $user = unserialize($_SESSION['user']);
+                        $carrito = $mongo_db->exec(
+                            'find_one',
+                            'users',
+                            ['_id' => new MongoDB\BSON\ObjectId(unserialize($_SESSION['user'])->id)]
+                        )->cart;
+                        foreach ($carrito as $prod_carrito) {
+                            if ($prod_carrito->selected) {
+                                $prod_info = $mongo_db->exec(
+                                    'find_one',
+                                    'products',
+                                    ['_id' => $prod_carrito->product]
+                                );
+                                $producerName = $mongo_db->exec(
+                                    'find_one',
+                                    'producers',
+                                    ['_id' => $prod_info->producer]
+                                )->company_name;
+                                $price = $prod_info->stock[$prod_carrito->size]->price;
+
+                                require(__DIR__ . './components/checkout/prod_checkout.php');
                             }
+                        }
                         ?>
                     </div>
                     <div id="resume_wrapper_total">
+                        <button class="checkout_btn">
+                            <span>Tramitar pedido</span>
+                            <img src="/DAW_proyecto_final/assets/icons/arrow_fwd.svg" alt="Flecha hacia delante">
+                        </button>
                         <div>
-                            <button id="checkout_btn">
-                                <span>Tramitar pedido</span>
-                                <img src="/DAW_proyecto_final/assets/icons/arrow_fwd.svg" alt="Flecha hacia delante">
-                            </button>
-                        </div>
-                        <div>
-                            <div>
-                                <span>Importe total:</span>
-                                <span>
-                                    <?php 
-                                        $product_price_sum = 0;
-                                        $total_price = 0;
-                                        foreach($carrito as $prod_carrito){
-                                            if($prod_carrito->selected){
-                                                $product_price_sum = $price * $prod_carrito->qty;
-                                                $total_price = $total_price + $product_price_sum;
-                                            }
-                                        }
+                            <p>
+                                Importe total:
+                                <?php
+                                $product_price_sum = 0;
+                                $total_price = 0;
+                                foreach ($carrito as $prod_carrito) {
+                                    if ($prod_carrito->selected) {
+                                        $product_price_sum = $price * $prod_carrito->qty;
+                                        $total_price = $total_price + $product_price_sum;
+                                    }
+                                }
 
-                                        echo $total_price . '€';
-                                    ?>
-                                </span>
-                            </div>
-                            <div>
-                                <span>Al completar tu pedido aceptas nuestras <a href="">Condiciones de uso y venta</a></span>
-                                <span>Consulta nuestro <a href="">Aviso de privacidad</a> </span>
-                                <span>y nuestro <a href="">Aviso de cookies</a> </span>
-                            </div>
+                                echo $total_price . '€';
+                                ?>
+                            </p>
+                            <p class="legal_text">
+                                Al completar tu pedido aceptas nuestras
+                                <a href="">Condiciones de uso y venta.</a>
+                                Consulta nuestro
+                                <a href="">Aviso de privacidad</a>
+                                y nuestro
+                                <a href="">Aviso de cookies</a>
+                            </p>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis a dicta mollitia provident. Corporis, et cupiditate ut itaque a magni laudantium, labore iusto, molestiae maiores provident nostrum deserunt impedit?
-                        Totam quos, molestiae, recusandae laborum quam iste cumque itaque cum adipisci sit similique. Voluptatem suscipit nulla molestiae pito doloremque saepe perspiciatis expedita, quae sint voluptatibus, quaerat deleniti voluptates! Omnis assumenda excepturi non amet sequi iure corporis voluptatibus enim corrupti aliquid, modi similique repellendus.
-                    </p>
                 </div>
             </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis a dicta mollitia provident. Corporis, et cupiditate ut itaque a magni laudantium, labore iusto, molestiae maiores provident nostrum deserunt impedit?
+                Totam quos, molestiae, recusandae laborum quam iste cumque itaque cum adipisci sit similique. Voluptatem suscipit nulla molestiae pito doloremque saepe perspiciatis expedita, quae sint voluptatibus, quaerat deleniti voluptates! Omnis assumenda excepturi non amet sequi iure corporis voluptatibus enim corrupti aliquid, modi similique repellendus.
+            </p>
         </div>
         <div id="col_right">
-            <div id="content_right">
-                <div class="section_content_right">
-                    <button id="checkout_btn">
-                        <span>Tramitar pedido</span>
-                        <img src="/DAW_proyecto_final/assets/icons/arrow_fwd.svg" alt="Flecha hacia delante">
-                    </button>
-                    <div>
-                        <span>Al completar tu pedido aceptas nuestras <a href="">Condiciones de uso y venta</a></span>
-                        <span>Consulta nuestro <a href="">Aviso de privacidad</a> y nuestro <a href="">Aviso de cookies</a></span>
+            <div id="res_cta" class="section_content_right">
+                <button class="checkout_btn">
+                    <span>Tramitar pedido</span>
+                    <img src="/DAW_proyecto_final/assets/icons/arrow_fwd.svg" alt="Flecha hacia delante">
+                </button>
+                <p class="legal_text">
+                    Al completar tu pedido aceptas nuestras
+                    <a href="">Condiciones de uso y venta.</a>
+                    Consulta nuestro
+                    <a href="">Aviso de privacidad</a>
+                    y nuestro
+                    <a href="">Aviso de cookies</a>
+                </p>
+            </div>
+            <div id="res_desglose" class="section_content_right">
+                <h3>Resumen de pedido:</h3>
+                <div class="section_content_right_tot">
+                    <p>Productos:</p>
+                    <div class="num_prices">
+                        <?php
+                        echo $total_price . " €";
+                        ?>
                     </div>
                 </div>
-                <div class="section_content_right">
-                    <h3>Resumen de pedido:</h3>
-                    <div class="section_content_right_tot">
-                        <h4>Productos:</h4>
-                        <div class="num_prices">
-                            <?php
-                                echo $total_price . " €";
-                            ?>
-                        </div>
-                    </div>
-                    <div class="section_content_right_tot">
-                        <h4>Envío:</h4>
-                        <div class="num_prices">
-                            <span>Loquesea</span>
-                        </div>
+                <div class="section_content_right_tot">
+                    <p>Envío:</p>
+                    <div class="num_prices">
+                        <!-- TODO -->
+                        <span>Loquesea</span>
                     </div>
                 </div>
-                <div class="section_content_right">
-                    <div class="section_content_right_tot">
-                        <h3>Importe total:</h3>
-                        <div  class="num_prices">
-                            <?php
-                                $envio = 3.95;
-                                echo round((($total_price + $envio) * 1.21), 2) . " €";
-                            ?>
-                        </div>
+            </div>
+            <div id="res_totals" class="section_content_right">
+                <div class="section_content_right_tot">
+                    <h3>Importe total:</h3>
+                    <div class="num_prices">
+                        <?php
+                        $envio = 3.95;
+                        echo round((($total_price + $envio) * 1.21), 2) . " €";
+                        ?>
                     </div>
-                    <br>
-                    <br>
-                    <span>Los totales de los pedidos incluyen IVA</span>
                 </div>
-                <div class="section_content_right_text">
-                    <p>Texto explicativo del proceso de pago, envío del pedido y entrega; posibles soluciones a los problemas y dudas frecuentes.</p>
-                </div>
+                <span>Los totales de los pedidos incluyen IVA</span>
+            </div>
+            <div class="section_content_right_text">
+                <p>Texto explicativo del proceso de pago, envío del pedido y entrega; posibles soluciones a los problemas y dudas frecuentes.</p>
             </div>
         </div>
     </div>
--- templates/components/checkout/prod_checkout.php
+++ templates/components/checkout/prod_checkout.php
@@ -1,10 +1,23 @@
 <div class="prod_checkout">
     <div class="prod_checkout_header">
-        <span>
-            Fecha de entrega estimada: <?php echo "" ?>
-        </span>
-        <span>
-            Si realizas el pedido antes de XX horas XX minutos
+        <span>Fecha de entrega estimada: <?php
+                                            $fecha = new DateTime('now');
+                                            $fecha->modify('+2 day');
+                                            echo $fecha->format('j M');
+                                            ?></span>
+        <span><?php
+                date_default_timezone_set('Europe/Madrid');
+
+                $time_now = time();
+                $end_of_day = strtotime('today 23:59:59');
+                $time_left = $end_of_day - $time_now;
+
+                // Convertir la diferencia de tiempo en horas y minutos
+                $hours_left = gmdate('H', $time_left);
+                $minutes_left = gmdate('i', $time_left);
+
+                echo "Si realizas el pedido antes de $hours_left horas $minutes_left minutos";
+                ?>
         </span>
     </div>
     <div class="prod_checkout_info">
@@ -12,9 +25,7 @@
         <div class="cart_text_wrap">
             <span class='name'><?php echo $prod_info->name ?></span>
             <span class="producer">Vendido por: <?php echo $producerName ?></span>
-            <br>
             <span class="stock"><?php echo $prod_info->stock[$prod_carrito->size]->qty > 0 ? 'En stock' : 'Agotado'; ?></span>
-            <br>
             <span class="price"><?php echo $price ?>€</span>
             <div class="prod_qty_wrapper">
                 <span>Cant.:&nbsp;</span>
@@ -22,21 +33,18 @@
                 <img src="/DAW_proyecto_final/assets/icons/expand.svg" alt="">
             </div>
         </div>
-        <div>
+        <div class="shipping">
             <span>Elige un método de envío:</span>
             <div>
-                <input type="radio" name="shipping_method1" id="">
                 <label for="shipping_method1">
-                    <span><strong>GRATIS</strong> A partir de 60€</span>
-                    <span>--</span>
-                    <span><?php echo "Establecer aqui fecha con PHP" ?></span>
+                    <input type="radio" name="shipping_method" id="shipping_method1" value="free">
+                    <span><strong>GRATIS</strong> A partir de 60€ -- <?php $texto = "recíbelo el " . date("l, d \\d\\e F");
+                                                                        echo $texto; ?></span>
                 </label>
-                <br>
-                <input type="radio" name="shipping_method2" id="" selected>
                 <label for="shipping_method2">
-                    <span><strong>3,95€ Envío Express</strong></span>
-                    <span>--</span>
-                    <span><?php echo "Establecer aqui fecha con PHP" ?></span>
+                    <input type="radio" name="shipping_method" id="shipping_method2" value="express" checked>
+                    <span><strong>3,95€ Envío Express</strong> -- <?php $texto = "recíbelo el " . date("l, d \\d\\e F", strtotime("+2 days"));
+                                                                    echo $texto; ?></span>
                 </label>
             </div>
         </div>
